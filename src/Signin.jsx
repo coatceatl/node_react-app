e0497fe9cf9bd8f62dd19a3ca2443b62096d06dc
Oksana Tkachenko <oksana@crystax.net>
2018-01-21 20:55:39 +0200

    changed components

--- src/Signin.jsx
+++ src/Signin.jsx
@@ -1,8 +1,37 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import BodyParser from 'body-parser';
+import axios from 'axios';
 
 class Signin extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
+  signIn() {
+    axios.post('/signin', {
+      email: this.state.email,
+      password: this.state.password
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  handleEmailChange(event) {
+    this.setState({ email:event.target.value })
+  }
+  handlePasswordChange(event) {
+    this.setState({ password:event.target.value })
+  }
+
   render() {
     return (
       <div>
@@ -10,6 +39,26 @@ class Signin extends Component {
           <h2 className='form-signin-heading'>
             Please Sign In
           </h2>
+          <input
+            type='email'
+            onChange={this.handleEmailChange}
+            placeholder='E-mail'
+            value={this.state.email}
+            required
+          />
+          <input
+            type='password'
+            onChange={this.handlePasswordChange}
+            placeholder='Password'
+            value={this.state.password}
+            required
+          />
+          <button className='btn btn-lg'
+            onClick={this.signIn}
+            type='button'
+          >
+            Sign in
+          </button>
         </form>
       </div>
     );
--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,8 +1,37 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import BodyParser from 'body-parser';
+import axios from 'axios';
 
 class Signup extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
+  signUp() {
+    axios.post('/signup', {
+      email: this.state.email,
+      password: this.state.password
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  handleEmailChange(event) {
+    this.setState({ email:event.target.value })
+  }
+  handlePasswordChange(event) {
+    this.setState({ password:event.target.value })
+  }
+
   render() {
     return (
       <div>
@@ -10,6 +39,26 @@ class Signup extends Component {
           <h2 className='form-signin-heading'>
             Please Sign Up
           </h2>
+          <input
+            type='email'
+            onChange={this.handleEmailChange}
+            placeholder='E-mail'
+            value={this.state.email}
+            required
+          />
+          <input
+            type='password'
+            onChange={this.handlePasswordChange}
+            placeholder='Password'
+            value={this.state.password}
+            required
+          />
+          <button className='btn btn-lg'
+            onClick={this.signIn}
+            type='button'
+          >
+            Sign in
+          </button>
         </form>
       </div>
     );
