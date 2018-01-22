1a853489eb0f0ffb9b79915d5f0b700c25fe88bd
Oksana Tkachenko <oksana@crystax.net>
2018-01-22 13:43:56 +0200

    added cookies

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
-const bodyParser = require('body-parser');
+const express       = require('express');
+const mongoose      = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport      = require('passport');
+const path          = require('path');
+const bodyParser    = require('body-parser');
+const keys          = require('./serverside/config/keys');
 require('./serverside/models/User');
 require('./serverside/services/passport.js');
 
 mongoose.connect('mongodb://localhost:27017/users');
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./serverside/routes/authRoutes.js')(app);
 
